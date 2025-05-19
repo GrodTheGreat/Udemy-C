dbdf9f8bceb9685158d6ece65b3ec6225c03316f
Gerard Causse <grod.causse@gmail.com>
2025-05-19 18:56:13 -0400

    Complete challenge attempt

---
+++ Variables/src/challenge.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    enum Company { GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT };
+
+    enum Company xerox = XEROX;
+    enum Company google = GOOGLE;
+    enum Company ebay = EBAY;
+
+    printf("%d\n%d\n%d\n", xerox, google, ebay);
+
+    return 0;
+}
