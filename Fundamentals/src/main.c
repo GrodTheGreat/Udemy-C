1cd189c5e85236a35e091cec2b04a83fecc95d5f
Gerard Causse <grod.causse@gmail.com>
2025-05-19 16:21:08 -0400

    Complete Comments and Header Files lessons

---
+++ Fundamentals/src/main.c
@@ -0,0 +1,15 @@
+// Preprocessor statements are indicated by the # symbol
+#include <stdio.h> // .h indicates a header file
+// # When an include has <> it usually means it's part of the standard library
+// When an include uses "" it usually means it's a custom file in the project directory
+
+// Preprocessors can also be used to define constants and macros
+#define PI 3.14;
+
+int main() {
+    //This is a single line comment
+    /*
+    This is a multi line comment
+    */
+    return 0; // You can add comments here too
+}
