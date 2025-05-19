7ac764a7562ce40e7af7bff034f55cf7746325cd
Gerard Causse <grod.causse@gmail.com>
2025-05-19 18:22:04 -0400

    Complete Data Types lesson

--- Variables/src/main.c
+++ Variables/src/main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h> // Required for true/false, otherwise you should use 1/0
 
 int main() {
     int jason; // Variable declared but not initialized
     jason = 5; // Variable initialized
 
     int bob = 7; // Variable declared and initialized
+
+    float mason = 23.22;
+    double jason2 = 55.5555555555555;
+    jason2 = 55.555555e+11;
+
+    _Bool boolVariable = true;
+    boolVariable = 1;
+    bool anotherBool = false; //bool needs stdbool otherwise it needs to be _Bool
 
     return 0;
 }
