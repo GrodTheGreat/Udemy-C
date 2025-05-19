2e336f73ceeaa23ada47225e84cd09cae3df4b07
Gerard Causse <grod.causse@gmail.com>
2025-05-19 18:00:03 -0400

    Complete variables lesson

---
+++ Variables/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int jason; // Variable declared but not initialized
+    jason = 5; // Variable initialized
+
+    int bob = 7; // Variable declared and initialized
+
+    return 0;
+}
