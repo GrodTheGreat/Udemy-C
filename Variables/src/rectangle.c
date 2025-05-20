eee611bf2353d9e14afed3ab4df5f20a5deed506
Gerard Causse <grod.causse@gmail.com>
2025-05-19 22:13:03 -0400

    Add command line options to challenge

--- Variables/src/rectangle.c
+++ Variables/src/rectangle.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    double width = 7.1;
-    double height = 6.8;
+int main(int argc, char *argv[]) {
+    double width = atoi(argv[1]);
+    double height = atoi(argv[2]);
     double area = 0.0;
     double perimeter = 0.0;
 
     perimeter = (width + height) * 2;
     area = width * height;
 
-    printf("Width: %f\nHeight: %f\nArea: %f\nPerimeter: %f\n", width, height, area, perimeter);
+    printf("Width: %.2f\nHeight: %.2f\nArea: %.2f\nPerimeter: %.2f\n", width, height, area, perimeter);
 
     return 0;
 }
